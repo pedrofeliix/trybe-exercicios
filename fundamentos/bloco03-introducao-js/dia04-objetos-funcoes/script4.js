13a4c0d6f294804becb5946af623029d096bec11
Pedro Felix <pedrofgoncalves14@gmail.com>
2023-01-26 15:41:32 -0300

    adicionado conteudo do dia 04

---
+++ fundamentos/bloco03-introducao-js/dia04-objetos-funcoes/script4.js
@@ -0,0 +1,28 @@
+const a = 214;
+const b = 65;
+
+function adicao(a, b) { 
+    return a + b;
+}
+
+function subtracao(a, b) {
+    return a - b;
+}
+
+function multiplicacao(a, b) {
+    return a * b;
+}
+
+function divisao(a, b) {
+    return a / b;
+}
+
+function modulo(a, b) {
+    return a % b;
+}
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
