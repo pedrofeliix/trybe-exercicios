df74a6b97e692772ba98f68fdfb9d398cbbb5a94
Pedro Felix <pedrofgoncalves14@gmail.com>
2023-01-27 15:27:35 -0300

    conclusao da aula e inicio de exercicios

---
+++ fundamentos/bloco03-introducao-js/dia05-javascript-ES6/aula.js
@@ -0,0 +1,30 @@
+/* if (true) {
+    // inicio do escopo do if
+    var userAge = '20';
+    console.log(userAge); // 20
+    // fim do escopo do if
+  }
+  console.log(userAge); // 20 */
+
+
+/*  _____________________________________________________________________________________________________________________________ */
+
+/* const favoriteLanguage = 'Javascript';
+favoriteLanguage = 'Python';
+console.log(favoriteLanguage); // Erro */
+
+/* let favoriteTechnology = 'Machine learning';
+favoriteTechnology = 'Facial recognition';
+console.log(favoriteTechnology); // Facial recognition */
+
+
+/*  _____________________________________________________________________________________________________________________________ */
+
+/* // Com o template literals
+console.log(`Primeira linha;
+Segunda linha;
+Terceira linha;`
+);
+
+// Sem o template literals:
+console.log('Primeira linha;\n' + 'Segunda linha;\n' + 'Terceira linha;\n'); */
---
+++ fundamentos/bloco03-introducao-js/dia05-javascript-ES6/exercicios.js
@@ -0,0 +1,96 @@
+/* Modifique a variável para que respeite o escopo onde está sendo declarada. Lembre-se: para que a variável respeite o escopo, ela não deve ser acessível fora do escopo em que esteja sendo declarada.
+
+Copie o código abaixo e rode-o para verificar sua saída: */
+
+/* function imprimeIdade() {
+    for (let idade = 30; idade <= 40; idade += 1) {
+      console.log('Idade dentro do for:', idade)
+    }
+  }
+  imprimeIdade(); */
+
+
+
+
+/* Altere o valor das propriedades do objeto, para que respeite as características da variável do tipo */
+
+/* // Executando esse código iremos receber um erro `TypeError: Assignment to constant variable.`
+let pessoa = {
+    nome: 'Henri',
+    idade: 20
+}
+pessoa.nome = 'Luna';
+pessoa.idade = 19;
+
+// Altere essa estrutura para corrigir o erro.
+console.log('Nome:', pessoa.nome);
+console.log('Idade:', pessoa.idade); */
+
+
+
+
+
+/* Modifique a variável para que não ocorra Erro; */
+
+/* let favoriteFood = 'Lasanha';
+favoriteFood = 'Hamburguer';
+console.log(favoriteFood); */
+
+
+
+
+/* Modifique as concatenações para template literals. */
+
+/* const name = 'Adriana';
+const lastName = 'Soares';
+console.log(`Olá ${name} ${lastName}`);
+function soma(a,b) {
+  let resultado = a + b;
+  return resultado;
+}
+let a = 3;
+let b = 5;
+console.log(`O resultado da soma de ${a} + ${b} é: ${soma(a,b)}`); */
+
+
+
+
+
+/* Modifique a estrutura das funções a seguir para que elas sejam arrow functions; */
+
+/* const numeroAleatorio = () => {return Math.random()}
+
+  console.log(numeroAleatorio()); */
+
+
+
+
+/*   Transforme a função hello em uma arrow function; */
+
+/* const hello = (nome) => {return `Olá, ${nome}!`}
+  let nome = 'Ivan';
+  console.log(hello(nome)); */
+
+
+
+
+
+  /* Transforme a função nomeCompleto em uma arrow function; */
+
+ /* const nomeCompleto = (nome, sobrenome) => {return `${nome} ${sobrenome}`}
+  let nome = 'Ivan';
+  let sobrenome = 'Pires';
+  console.log(nomeCompleto(nome, sobrenome)); */
+
+
+
+
+
+
+  /* Altere a expressão if/else utilizando ternary operator; */
+
+  let speed = 180;
+  const speedCar = (speed) => (
+    speed >= 120 ? `Você ultrapassou o limite de velocidade` : `Você está na velocidade permitida`
+  );
+  console.log(speedCar(speed));
