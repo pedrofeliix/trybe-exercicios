db3ca9174aa487cc0675ca87b92ca5cc316aaafa
Pedro Felix <pedrofgoncalves14@gmail.com>
2023-02-02 09:55:29 -0300

    conteudo dia 02

---
+++ fundamentos/bloco04-js-dom-eventos-web-storage/dia02-js-elementos/script.js
@@ -0,0 +1,5 @@
+console.log(document.getElementById('.elementoOndeVoceEsta'));
+
+console.log(document.getElementById('.pai').style.color) = 'blue';
+
+console.log(document.getElementById('.primeiroFilhoDoFilho').innerText) = 'Adicionando texto';
